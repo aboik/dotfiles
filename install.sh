86ed54bab0ad2c3735a4965e52369757408bac02
Andrew Boik <dboik@cisco.com>
2015-04-09 15:32:21 -0400

    Install vimrc and gitconfig

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
-#!/bin/sh
-
+#!/bin/bash
+SCRIPT=$(readlink -f "$0")
+SCRIPT_PATH=$(dirname "$SCRIPT")
+install_vimrc()
+{
+	cp $SCRIPT_PATH/.vimrc ~/.vimrc
+}
 install_pathogen()
 {
 	mkdir -p ~/.vim/autoload ~/.vim/bundle
@@ -10,5 +15,16 @@ install_vim_colors_solarized()
 	git clone http://github.com/altercation/vim-colors-solarized.git \
 		~/.vim/bundle/vim-colors-solarized
 }
-install_pathogen
-install_vim_colors_solarized
+install_vim()
+{
+	install_vimrc
+	install_pathogen
+	install_vim_colors_solarized
+}
+install_gitconfig_cisco()
+{
+	cp $SCRIPT_PATH/cisco/.gitconfig ~/.gitconfig
+}
+
+install_vim
+install_gitconfig_cisco
